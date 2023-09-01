09ed068cd5acacc785cd19fbf17b3ffb706d257c
Bruno <bruno.uemura1@gmail.com>
2023-09-01 18:57:50 -0300

    feat: removes gorm logging

--- internal/infra/database/connection.go
+++ internal/infra/database/connection.go
@@ -6,6 +6,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -13,7 +14,9 @@ var DB *gorm.DB
 func Connect() error {
 	var err error
 	dsn := os.Getenv("DATABASE_URL")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		log.Fatal("Failed to connect to database")
 	}
