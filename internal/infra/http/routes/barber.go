b26d9b03dc3764abb4468502c36c9fe99a0a767a
Bruno <bruno.uemura1@gmail.com>
2023-09-02 17:49:08 -0300

    feat: removes unnucessary auth middleware

--- internal/infra/http/routes/barber.go
+++ internal/infra/http/routes/barber.go
@@ -2,13 +2,12 @@ package routes
 
 import (
 	"github.com/fio-de-navalha/fdn-back/internal/infra/container"
-	"github.com/fio-de-navalha/fdn-back/internal/infra/http/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
 func setupBarberRouter(router fiber.Router) {
 	barbers := router.Group("/barbers")
-	barbers.Get("/:id", middlewares.EnsureAuth(), container.BarberHandler.GetById)
+	barbers.Get("/:id", container.BarberHandler.GetById)
 
 	auth := router.Group("/auth")
 	auth.Post("/register/barbers", container.BarberHandler.Register)
