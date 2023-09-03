7485b959b4ffbed4fa2a095aa5a153abc1cd90e5
buemura <bruno.uemura1@gmail.com>
2023-09-03 13:41:05 -0300

    feat: updates phone number maximum length

--- internal/domain/customer/customer_input.go
+++ internal/domain/customer/customer_input.go
@@ -2,12 +2,12 @@ package customer
 
 type CustomerInput struct {
 	Name     string `json:"name" validate:"required,min=3,max=30"`
-	Phone    string `json:"phone" validate:"required,min=9,max=12"`
+	Phone    string `json:"phone" validate:"required,min=9,max=15"`
 	Password string `json:"password" validate:"required,min=6"`
 }
 
 type LoginInput struct {
-	Phone    string `json:"phone" validate:"required,min=9,max=12"`
+	Phone    string `json:"phone" validate:"required,min=9,max=15"`
 	Password string `json:"password" validate:"required,min=6"`
 }
 
