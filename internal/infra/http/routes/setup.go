a92b060e3c1ec983af1bbfe344e253615e6d2e32
Bruno <bruno.uemura1@gmail.com>
2023-09-03 18:12:03 -0300

    feat: removes metrics route

--- internal/infra/http/routes/setup.go
+++ internal/infra/http/routes/setup.go
@@ -2,12 +2,9 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
 func FiberSetupRouters(app *fiber.App) {
-	app.Get("/metrics", monitor.New(monitor.Config{Title: "Backend Metrics Page"}))
-
 	router := app.Group("/api")
 
 	setupHealthRouter(router)
