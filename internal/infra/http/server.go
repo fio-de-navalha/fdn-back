d870444181584ba5de97f0955810fb7599dda776
Bruno <bruno.uemura1@gmail.com>
2023-09-03 15:48:53 -0300

    feat: adds fiber middlewares

--- internal/infra/http/server.go
+++ internal/infra/http/server.go
@@ -2,21 +2,45 @@ package http
 
 import (
 	"os"
+	"time"
 
 	"github.com/fio-de-navalha/fdn-back/internal/infra/http/routes"
 	"github.com/gofiber/contrib/swagger"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/idempotency"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func Server() {
 	app := fiber.New()
 
-	app.Use(recover.New())
+	app.Use(idempotency.New())
+	app.Use(limiter.New(limiter.Config{
+		Max:               15,
+		Expiration:        30 * time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	}))
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("refresh") == "true"
+		},
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
+	app.Use(recover.New(recover.Config{
+		EnableStackTrace: true,
+	}))
 	app.Use(cors.New())
-	app.Use(logger.New())
+	app.Use(logger.New(logger.Config{
+		Format: "${time} | ${ip}:${port} | ${latency} | ${status} | ${method} ${path}\n",
+	}))
+
+	app.Get("/metrics", monitor.New(monitor.Config{Title: "Backend Metrics Page"}))
 
 	routes.FiberSetupRouters(app)
 
