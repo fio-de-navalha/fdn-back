d1cf4553e8eadf28c1f73eb9c47d2d09f72c5111
Bruno <bruno.uemura1@gmail.com>
2023-09-02 22:22:54 -0300

    feat: adds cors

--- internal/infra/http/server.go
+++ internal/infra/http/server.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/fio-de-navalha/fdn-back/internal/infra/http/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func Server() {
 	app := fiber.New()
+	app.Use(cors.New())
 	app.Use(logger.New())
 
 	routes.FiberSetupRouters(app)
