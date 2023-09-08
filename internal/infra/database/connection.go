f620a1f073f091667b51305129506a4e430e1851
Bruno <bruno.uemura1@gmail.com>
2023-09-08 10:29:47 -0300

    feat: disable gorm logger

--- internal/infra/database/connection.go
+++ internal/infra/database/connection.go
@@ -6,6 +6,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 )
 
@@ -15,6 +16,7 @@ func Connect() error {
 	var err error
 	dsn := os.Getenv("DATABASE_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true,
 		},
