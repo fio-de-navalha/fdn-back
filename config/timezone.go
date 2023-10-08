d561b1b1045188fb198cec1a9d9261de53eb9500
Bruno <bruno.uemura1@gmail.com>
2023-10-08 14:44:47 -0300

    fix: timezone gmt-3

--- config/timezone.go
+++ config/timezone.go
@@ -6,11 +6,7 @@ import (
 )
 
 func setupTimezone() {
-	location, err := time.LoadLocation("GMT-3")
-	if err != nil {
-		fmt.Println("Error loading timezone:", err)
-		return
-	}
+	location := time.FixedZone("GMT-3", -3*60*60)
     time.Local = location
     currentTime := time.Now()
     fmt.Println("Current time in GMT-3:", currentTime.Format(time.RFC3339))
