6b9ba69bdf2ca93b44123fa77aa5077d891e88df
Bruno <bruno.uemura1@gmail.com>
2023-10-11 22:22:00 -0300

    fix: adds logs

--- config/timezone.go
+++ config/timezone.go
@@ -5,8 +5,11 @@ import (
 	"time"
 )
 
+var GMTMinus3 *time.Location
+
 func setupTimezone() {
     time.Local, _ = time.LoadLocation("America/Sao_Paulo")
+    GMTMinus3 = time.Local
     currentTime := time.Now()
     fmt.Println("Current timezone:", time.Local)
     fmt.Println("Current time:", currentTime.Format(time.RFC3339))
